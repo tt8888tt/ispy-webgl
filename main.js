9cf6b66a658489c17ee53f98491707f74c1e8049
tpmccauley <tpmccauley@gmail.com>
2015-02-18 18:38:15 +0000

    add code for packaging via atom-shell

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+// This method will be called when atom-shell has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1200, height: 1000});
+
+  // and load the index.html of the app.
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
